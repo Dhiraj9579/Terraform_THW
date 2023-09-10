2d2ffaa49b69f2d2ddf1f6171086daf0c3602f74
Dhiraj <dhirajahire957@outlook.com>
2023-09-10 09:54:09 -0700

    main commit

--- main.tf
+++ main.tf
@@ -1,83 +1,89 @@
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "mediawikiTHW" {
+  name     = "mediawikiTHW-resources"
+  location = "East US"
 }
 
-resource "azurerm_resource_group" "mediawiki" {
-  name     = "mediawiki-rg"
-  location = "East US"  # Change this to your desired Azure region
-}
-
-resource "azurerm_virtual_network" "mediawiki" {
-  name                = "mediawiki-network"
+resource "azurerm_virtual_network" "mediawikiTHW" {
+  name                = "mediawikiTHW-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.mediawiki.location
-  resource_group_name = azurerm_resource_group.mediawiki.name
+  location            = azurerm_resource_group.mediawikiTHW.location
+  resource_group_name = azurerm_resource_group.mediawikiTHW.name
 }
 
-resource "azurerm_subnet" "mediawiki" {
-  name                 = "mediawiki-subnet"
-  resource_group_name  = azurerm_resource_group.mediawiki.name
-  virtual_network_name = azurerm_virtual_network.mediawiki.name
+resource "azurerm_subnet" "mediawikiTHW" {
+  name                 = "mediawikiTHW-subnet"
+  resource_group_name  = azurerm_resource_group.mediawikiTHW.name
+  virtual_network_name = azurerm_virtual_network.mediawikiTHW.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "mediawiki" {
-  name                = "mediawiki-nsg"
-  location            = azurerm_resource_group.mediawiki.location
-  resource_group_name = azurerm_resource_group.mediawiki.name
+resource "azurerm_network_security_group" "mediawikiTHW" {
+  name                = "mediawikiTHW-nsg"
+  location            = azurerm_resource_group.mediawikiTHW.location
+  resource_group_name = azurerm_resource_group.mediawikiTHW.name
 }
 
-resource "azurerm_network_security_rule" "mediawiki" {
-  name                        = "allow-http"
-  priority                    = 100
+resource "azurerm_network_security_rule" "mediawikiTHW" {
+  name                        = "SSH"
+  priority                    = 1001
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "80"
+  destination_port_range      = "22"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.mediawiki.name
-  network_security_group_name = azurerm_network_security_group.mediawiki.name
+  resource_group_name         = azurerm_resource_group.mediawikiTHW.name
+  network_security_group_name = azurerm_network_security_group.mediawikiTHW.name
 }
 
-resource "azurerm_network_interface" "mediawiki" {
-  name                = "mediawiki-nic"
-  location            = azurerm_resource_group.mediawiki.location
-  resource_group_name = azurerm_resource_group.mediawiki.name
+resource "azurerm_subnet_network_security_group_association" "mediawikiTHW" {
+  subnet_id                 = azurerm_subnet.mediawikiTHW.id
+  network_security_group_id = azurerm_network_security_group.mediawikiTHW.id
+}
+
+resource "azurerm_public_ip" "mediawikiTHW" {
+  name                = "mediawikiTHW-pip"
+  location            = azurerm_resource_group.mediawikiTHW.location
+  resource_group_name = azurerm_resource_group.mediawikiTHW.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "mediawikiTHW" {
+  name                = "mediawikiTHW-nic"
+  location            = azurerm_resource_group.mediawikiTHW.location
+  resource_group_name = azurerm_resource_group.mediawikiTHW.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.mediawiki.id
+    name                          = "mediawikiTHW-nic-configuration"
+    subnet_id                     = azurerm_subnet.mediawikiTHW.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id           = azurerm_public_ip.mediawikiTHW.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "mediawiki" {
-  name                = "mediawiki-vm"
-  location            = azurerm_resource_group.mediawiki.location
-  resource_group_name = azurerm_resource_group.mediawiki.name
-  size                = "Standard_DS2_v2" # Choose an appropriate VM size
-  admin_username      = "adminuser"      # Change this to your desired username
+resource "azurerm_linux_virtual_machine" "mediawikiTHW" {
+  name                = "mediawikiTHW-vm"
+  location            = azurerm_resource_group.mediawikiTHW.location
+  resource_group_name = azurerm_resource_group.mediawikiTHW.name
+  network_interface_ids = [azurerm_network_interface.mediawikiTHW.id]
 
-  network_interface_ids = [azurerm_network_interface.mediawiki.id]
+  size                  = "Standard_DS2_v2"
+  admin_username        = "azureuser"
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("mediawiki.pub") # Replace with the path to your SSH public key
+  }
 
   os_disk {
-    name              = "mediawiki-osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Choose an appropriate storage type
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8_4"
     version   = "latest"
   }
-
-  admin_ssh_key {
-    username   = "adminuser"                # Change this to your desired username
-    public_key = file("~/.ssh/id_rsa.pub")  # Use your public SSH key
-  }
 }
-
--- providers.tf
+++ providers.tf
@@ -1,3 +1,7 @@
-provider "azuread" {
-tenant_id = "edf442f5-b994-4c86-a131-b42b03a16c95"
+provider "azurerm" {
+  features {}
+  client_id       = "55052412-7f84-4303-a970-a902e73fda13"
+  client_secret   = "sDD8Q~PON4pLc7tcaGDW5Sd14KCSwm~MYdSyHbPW"
+  subscription_id = "be4e18f7-cea5-4dd9-84cb-347a164cbd5d"
+  tenant_id       = "a4074918-2784-47cd-a664-4c81f6b67d1e"
 }
