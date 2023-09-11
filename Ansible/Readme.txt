e902937b3ab00b04e60a7646ca63815ed1280f1a
Dhiraj <dhirajahire957@outlook.com>
2023-09-11 04:48:23 -0700

    main commit

---
+++ Ansible/Readme.txt
@@ -0,0 +1,4 @@
+Command  used for execution of Ansible:
+ansible-playbook -i /mnt/f/Ansible/inventory.ini mediawiki7.yaml --private-key /mnt/f/Terraform/mediawiki
+
+inventory.ini - File is  updated with username and remote IP address of the Azure VM
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  client_id       = "55052412-7f84-4303-a970-a902e73fda13"
-  client_secret   = "sDD8Q~PON4pLc7tcaGDW5Sd14KCSwm~MYdSyHbPW"
-  subscription_id = "be4e18f7-cea5-4dd9-84cb-347a164cbd5d"
-  tenant_id       = "a4074918-2784-47cd-a664-4c81f6b67d1e"
+  client_id       = "5505241*****0-a902e73fda13"
+  client_secret   = "s******CSwm~MYdSyHbPW"
+  subscription_id = "be4e18******cb-347a164cbd5d"
+  tenant_id       = "a407491******-4c81f6b67d1e"
 }
